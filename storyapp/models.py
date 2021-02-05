fe77e997f9b6bdafd7bcaccba5a90ec9e90ebc70
Ringo Alcock <ringo.alcock@gmail.com>
2021-02-04 22:05:23 -0800

    add scene model

--- storyapp/models.py
+++ storyapp/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 # Create your models here.
 class Story(models.Model):
     title = models.CharField(max_length=200)
@@ -8,3 +9,12 @@ class Story(models.Model):
 
     def __str__(self):
         return self.title
+
+class Scene(models.Model):
+    entity_key = models.CharField(max_length=8)
+    content_blocks = models.TextField(blank=True)
+    card_summary = models.TextField(blank=True)
+    location = models.PositiveSmallIntegerField
+
+    def __str__(self):
+        return self.entity_key
