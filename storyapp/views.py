3730d5a01a353536f8db1094cc767efd8aef6eba
Ringo Alcock <ringo.alcock@gmail.com>
2021-02-09 20:14:28 -0800

    write new story and scene views with CRUD actions

--- storyapp/views.py
+++ storyapp/views.py
@@ -1,40 +1,86 @@
-from django.shortcuts import render
-from django.http import HttpResponse, JsonResponse
+from django.shortcuts import get_object_or_404
 from rest_framework import viewsets
 from rest_framework.response import Response
 
 from .serializers import StorySerializer, SceneSerializer
 from .models import Story, Scene
-from django.views.generic.list import ListView
 
 
-class StoryView(viewsets.ModelViewSet):
-    serializer_class = StorySerializer
-    queryset = Story.objects.all()
+class StoryView(viewsets.ViewSet):
+    def list(self, request):
+        queryset = Story.objects.all()
+        serializer = StorySerializer(queryset, many=True)
+        return Response(serializer.data)
 
-class SceneView(viewsets.ModelViewSet):
-    serializer_class = SceneSerializer
-    queryset = Scene.objects.all()
+    def retrieve(self, request, pk=None):
+        queryset = Story.objects.all()
+        story = get_object_or_404(queryset, pk=pk)
+        serializer = StorySerializer(story)
+        return Response(serializer.data)
 
-    # Scene.objects.filter(story__in=Story.objects.filter(id=id))
-    # Child.objects.filter(parent__in=Parent.objects.filter(name__startswith='A'))
-    # def get_queryset(self):
-    #     queryset = Scene.objects.all()
-    #     story = self.request.query_params.get('story')
-    #     queryset = queryset.filter(story=story)
-    #     return queryset
+    def create(self, request):
+        serializer = StorySerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response({"message":"success", "data":serializer.data})
 
-    # this doesn't work
-    # Object of type Scene is not JSON serializable
-    # def list(self, request):
-    #     story = self.request.query_params.get('story')
-    #     results = Scene.objects.filter(story=story)
-    #     return Response(results)
+    def update(self, request, pk=None):
+        try:
+            story = Story.objects.get(id=pk)
+            serializer = StorySerializer(story, data=request.data)
+            serializer.is_valid(raise_exception=True)
+            serializer.save()
+        except:
+            return Response({"message":"invalid story or request"})
+        return Response({"message":"successful", "data":serializer.data})
 
-    # or this
-    # AttributeError when attempting to get a value for field `entity_key` on serializer `SceneSerializer`.
-    # def list(self, request):
-    #     story = self.request.query_params.get('story')
-    #     result = Scene.objects.filter(story=story)
-    #     serializer = SceneSerializer(result, many=False)
-    #     return Response(serializer.data)
+    def destroy(self, request, pk=None):
+        queryset = Story.objects.all()
+        story = get_object_or_404(queryset, pk=pk)
+        serializer = StorySerializer(story, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        story.delete()
+        return Response({"message": "successful", "data":serializer.data})
+
+
+class SceneView(viewsets.ViewSet):
+    def list(self, request):
+        if request.query_params:
+            story = self.request.query_params.get('story')
+            queryset = Scene.objects.filter(story=story)
+            serializer = SceneSerializer(queryset, many=True)
+            return Response(serializer.data)
+        else:
+            queryset = Scene.objects.all()
+            serializer = SceneSerializer(queryset, many=True)
+            return Response(serializer.data)
+
+    def retrieve(self, request, pk=None):
+        queryset = Scene.objects.all()
+        scene = get_object_or_404(queryset, pk=pk)
+        serializer = SceneSerializer(scene)
+        return Response(serializer.data)
+
+    def create(self, request):
+        serializer = SceneSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response({"message":"success", "data":serializer.data})
+
+    def update(self, request, pk=None):
+        try:
+            scene = Scene.objects.get(id=pk)
+            serializer = SceneSerializer(scene, data=request.data)
+            serializer.is_valid(raise_exception=True)
+            serializer.save()
+        except:
+            return Response({"message":"invalid scene or request"})
+        return Response({"message":"successful", "data":serializer.data})
+
+    def destroy(self, request, pk=None):
+        queryset = Scene.objects.all()
+        scene = get_object_or_404(queryset, pk=pk)
+        serializer = SceneSerializer(scene, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        scene.delete()
+        return Response({"message":"successful", "data":serializer.data})
