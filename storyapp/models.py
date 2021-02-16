a5f85373386465999b5bfe851527a0ae636e53e7
Ringo Alcock <ringo.alcock@gmail.com>
2021-02-16 14:53:02 -0800

    sort story objects in descending last_updated order

--- storyapp/models.py
+++ storyapp/models.py
@@ -7,6 +7,9 @@ class Story(models.Model):
     last_updated = models.DateTimeField(auto_now=True)
     user = models.ForeignKey('User', on_delete=models.CASCADE, related_name='stories')
 
+    class Meta:
+        ordering = ('-last_updated', )
+
     def __str__(self):
         return self.title
 
