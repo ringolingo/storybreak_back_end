cbe9ed58f0d9de50d24d081c806923ddec156505
Ringo Alcock <ringo.alcock@gmail.com>
2021-02-15 18:00:08 -0800

    add heroku to whitelist

--- storybreak_back_end/settings.py
+++ storybreak_back_end/settings.py
@@ -127,7 +127,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 CORS_ORIGIN_WHITELIST = [
-     "http://localhost:3000"
+    "http://localhost:3000",
+    "http://storybreak.herokuapp.com"
 ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, "static/")
